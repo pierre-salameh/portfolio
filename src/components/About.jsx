2cbcba1d6cc948b5d5774da2986d573cc9f66ca9
Pierre-Salameh <pierresalam6@gmail.com>
2026-07-07 16:36:56 +0300

    Fix mobile star positionnnnnn

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,13 +10,13 @@ function About() {
 
         <p className="text-gray-300 text-lg text-center max-w-3xl mx-auto">
        Full Stack Web Developer with strong experience in React.js and Laravel, building 
-scalable web applications, dashboards, and automation systems.
-<br></br>
-I developed multiple real-world projects including management systems, authentication 
-platforms, and REST API integrations, focusing on performance and clean architecture.
-<br></br>
-Currently working as a React.js Instructor, mentoring developers and guiding them 
-to build production-ready applications.
+      scalable web applications, dashboards, and automation systems.
+      <br></br>
+      I developed multiple real-world projects including management systems, authentication 
+      platforms, and REST API integrations, focusing on performance and clean architecture.
+      <br></br>
+      Currently working as a React.js Instructor, mentoring developers and guiding them 
+      to build production-ready applications.
         </p>
 
       </div>
